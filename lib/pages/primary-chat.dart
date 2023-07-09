4e329d11f38ec0c3640a77b00f47fba5e3c80fd2
DusaVignesh <83420629+DusaVignesh@users.noreply.github.com>
2023-07-09 10:20:35 +0530

    initial commit

--- lib/pages/primary-chat.dart
+++ lib/pages/primary-chat.dart
@@ -1,5 +1,6 @@
 import 'package:basicapp/controller/controller.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 
 import '../data/data.dart';
@@ -29,6 +30,7 @@ class PrimaryChatState extends State<PrimaryChat> {
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return Scaffold(
+      extendBody: true,
       body: Container(
         padding: EdgeInsets.symmetric(vertical: 25, horizontal: 18),
         height: double.infinity,
@@ -69,17 +71,55 @@ class PrimaryChatState extends State<PrimaryChat> {
             SizedBox(
               height: height(size, 18),
             ),
+            // SizedBox(
+            //   width: double.infinity,
+            //   child: Obx(
+            //     () => ListView.builder(
+            //       shrinkWrap: true,
+            //       itemCount: controller.foundContacts.length,
+            //       itemBuilder: (context, index) =>
+            //           Text(controller.foundContacts[index]),
+            //     ),
+            //   ),
+            // ),
             SizedBox(
               width: double.infinity,
-              child: Obx(
-                () => ListView.builder(
-                  shrinkWrap: true,
-                  itemCount: controller.foundContacts.length,
-                  itemBuilder: (context, index) =>
-                      Text(controller.foundContacts[index]),
+              height: height(size, 542),
+              child: ListView.builder(
+                padding: EdgeInsets.zero,
+                shrinkWrap: true,
+                itemCount: users.length,
+                itemBuilder: (context, index) => InkWell(
+                  onTap: () => Get.toNamed('/primarychatWindow'),
+                  child: ListTile(
+                    contentPadding: EdgeInsets.zero,
+                    trailing:
+                        Text(userData[users[index]]["recent-msg"]["date"]),
+                    title: Padding(
+                      padding: const EdgeInsets.only(bottom: 11.0),
+                      child: Text(
+                        users[index],
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.w600),
+                      ),
+                    ),
+                    subtitle: Text(
+                      userData[users[index]]["recent-msg"]["msg"],
+                      maxLines: 1,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    leading: SizedBox(
+                      width: size.width * 0.12,
+                      height: size.height * 0.06,
+                      child: SvgPicture.asset(
+                        userData[users[index]]["profile"],
+                        fit: BoxFit.fill,
+                      ),
+                    ),
+                  ),
                 ),
               ),
-            )
+            ),
           ],
         ),
       ),
